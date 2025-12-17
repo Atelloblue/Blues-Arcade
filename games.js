c2dc7db8e6376501883cdf6e183bb12f90f34244
Atelloblue <137169870+Atelloblue@users.noreply.github.com>
2025-12-17 04:15:43 -0600

    Replace Sphere Dash with Snake.io in gameData

---
+++ games.js
@@ -0,0 +1,11 @@
+// games.js
+
+const gameData = [
+    {
+        name: "Snake.io",
+        description: "Grow your snake longer and faster than every other player in the arena!",
+        icon: "🐍",
+        category: "Strategy",
+        link: "games/snake-io.html"
+    } 
+]; 
